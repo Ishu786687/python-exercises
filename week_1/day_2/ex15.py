c56d3f4d54c687a7df024cae991d0a8f7561eb07
Ishu786687 <ishupal987@gail.com>
2024-04-05 22:03:56 +0530

    this is my first commit

--- week_1/day_2/ex15.py
+++ week_1/day_2/ex15.py
@@ -1,11 +1,11 @@
 from sys import argv
 
-script, filename = argv
+script, filename = argv   #storing program file name in script and file name in filename
 
-txt = open(filename)
+txt = open(filename)        #file object is created and stored in txt variable
 
 print("Here's your file %r:"%filename)
-print(txt.read())
+print(txt.read()) #read method reads entire file as string
 
 print("Type the filename again: ")
 file_again = input("> ")
@@ -13,3 +13,12 @@ file_again = input("> ")
 txt_again = open(file_again)
 
 print(txt_again.read())
+
+
+"""txt_again is a variable used to store the file object 
+    created when opening a file specified by the user's input 
+    (file_again). Let's break down how txt_again is used in the code:
+"""
+    
+txt.close()
+txt_again.close()
