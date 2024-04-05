9d5b5b05753cd3040ba5292affeaae047e5b9568
Ishu786687 <ishupal987@gail.com>
2024-04-05 11:23:48 +0530

    this is my first commit

---
+++ week_1/day_2/ex10.py
@@ -0,0 +1,23 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print("hyr\abye")
+print("hyr\bbye")
+print("hyr\fbye")
+print("hyr\N{mango}bye")
+print("hyr\vbye")
+
+# while True:
+#     for i in ["/","- ","|","\\","|"]:
+#          print("%s\r" % i)
