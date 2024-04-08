5bbbc4b063258383a3e13c59293b3bf276683a23
Ishu786687 <ishupal987@gail.com>
2024-04-08 12:26:11 +0530

    this is my first commit

--- week_1/day_4/ex35.py
+++ week_1/day_4/ex35.py
@@ -26,4 +26,52 @@ def bear_room():
         next = input(">")
         
         if next == "take honey":
+            dead("The bear looks at you and slaps your face off.")
+        elif next == "taunt bear" and not bear_moved:
+            print("The bear has moved from the door. You can go through it now.")
+            bear_moved = True
+        elif next == "taunt bear" and bear_moved:
+            print("The bear has moved from the floor. You can go through it now.")
+        elif next == "open door" and bear_moved:
+            gold_room()
+        else:
+            print("I got no idea what means.")
+
+def cthulhu_room():
+    print("Here you see the great evil Cthulhu.")
+    print("He, it, whatever stares at you and eat your head.")
+    print("Do you flee for your life or eat your head?")
+    
+    next = input("> ")
+    
+    if "flee" in next:
+        start()
+    elif "head" in next:
+        dead("Well that was tasty!")
+    else:
+        cthulhu_room()
+        
+def dead(why):
+    print(why, "Good job!")
+    exit(0)
+    
+def start():
+    print("You are in a dark room.")
+    print("There is a door to your right and left.")
+    print("Which one do you take?")
+    
+    next = input("> ")
+    
+    if next == "left":
+        bear_room()
+    elif next == "right":
+        cthulhu_room()
+    else:
+        dead("You stumble around the room until you starve.")
+        
+start()
+
+    
+            
+        
             
